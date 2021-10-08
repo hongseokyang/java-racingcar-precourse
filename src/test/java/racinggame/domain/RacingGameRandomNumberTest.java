b85c3dc5377dd7f8ad65342069efa93efe37e1eb
hongseok <yhongseok92@gmail.com>
2021-10-08 20:24:05 +0900

    test: 게임에 필요한 랜덤 숫자를 관리하는 기능의 테스트 코드 작성
    - 랜덤 숫자의 범위 검증 테스트
    - 자동차 전진 가능여부를 확인하는 기능 테스트

---
+++ src/test/java/racinggame/domain/RacingGameRandomNumberTest.java
@@ -0,0 +1,31 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingGameRandomNumberTest {
+    @Test
+    @DisplayName("범위보다 낮은 숫자로 볼 생성하기")
+    public void 랜덤숫자_생성_실패() {
+        assertThatThrownBy(() -> {
+            new RacingGameRandomNumber(10);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("랜덤 숫자는 0~9의 값이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false",
+            "9:true",
+            "3:false",
+            "4:true",
+    }, delimiter = ':')
+    void 앞으로_전진(int randomNumber, boolean expected) {
+        assertThat(new RacingGameRandomNumber(randomNumber).isPossibleMoveForward()).isEqualTo(expected);
+    }
+}
