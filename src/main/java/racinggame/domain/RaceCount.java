ef67595c4e7a94cb0fcbf746c557678bb6119582
hongseok <yhongseok92@gmail.com>
2021-10-09 12:14:58 +0900

    feat: 경주를 반복할 횟수를 생성하는 기능 추가
    - RaceCount

---
+++ src/main/java/racinggame/domain/RaceCount.java
@@ -0,0 +1,49 @@
+package racinggame.domain;
+
+import racinggame.utils.ValidationUtils;
+
+public class RaceCount {
+    private static final int MIN_NO = 1;
+
+    private int count;
+
+    public RaceCount(String count) {
+        validate(count);
+        this.count = Integer.parseInt(count);
+    }
+
+    public RaceCount(int count) {
+        validate(count);
+        this.count = count;
+    }
+
+    private void validate(String count) {
+        if (!isValid(count)) {
+            illegalArgumentException();
+        }
+    }
+
+    private void validate(int count) {
+        if (!isValid(count)) {
+            illegalArgumentException();
+        }
+    }
+
+    private void illegalArgumentException() {
+        throw new IllegalArgumentException("시도할 횟수는 양의 정수 값이어야 합니다.");
+    }
+
+    private boolean isValid(String count) {
+        return !ValidationUtils.isEmpty(count)
+                && ValidationUtils.isNumeric(count)
+                && isValid(Integer.parseInt(count));
+    }
+
+    private boolean isValid(int count) {
+        return ValidationUtils.inValidMin(count, MIN_NO);
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
