817ddb27384ef621129b37a462ec2a95a3f2e283
hongseok <yhongseok92@gmail.com>
2021-10-09 12:16:50 +0900

    feat: 자동차 이름을 생성하는 기능 추가
    - CarName

---
+++ src/test/java/racinggame/domain/RaceCountTest.java
@@ -0,0 +1,28 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RaceCountTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "0", "abc"})
+    void 반복횟수_생성_예외처리(String count) {
+        assertThatThrownBy(() -> {
+            new RaceCount(count);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수는 양의 정수 값이어야 합니다.");
+    }
+
+    @Test
+    void 반복횟수_생성_성공() {
+        RaceCount countInt = new RaceCount(5);
+        RaceCount countString = new RaceCount("5");
+
+        assertThat(5).isEqualTo(countInt.getCount());
+        assertThat(5).isEqualTo(countString.getCount());
+    }
+}
