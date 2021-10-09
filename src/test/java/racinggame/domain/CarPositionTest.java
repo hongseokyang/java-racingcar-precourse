f9ea2ef091246ac4c384f95a4ee1db478d478843
hongseok <yhongseok92@gmail.com>
2021-10-09 13:23:50 +0900

    test: 자동차 포지션을 관리하는 기능 테스트 코드 작성
    CarPositionTest
    - 자동차 포지션 비교

---
+++ src/test/java/racinggame/domain/CarPositionTest.java
@@ -0,0 +1,28 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarPositionTest {
+    CarPosition carPosition;
+
+    @BeforeEach
+    void setUp() {
+        carPosition = new CarPosition(5);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:-1",
+            "5:0",
+            "6:1"
+    }, delimiter = ':')
+    void 자동차_포지션_비교(int position, int expected) {
+        CarPosition target = new CarPosition(position);
+
+        assertThat(expected).isEqualTo(target.compareTo(carPosition));
+    }
+}
