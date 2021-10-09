817525f7edeae9bc5b98e435044dd5607d4db8fd
hongseok <yhongseok92@gmail.com>
2021-10-09 13:37:12 +0900

    refactor: 클래스명 변경
    - RandomRaceNumber -> RaceNumber
    - RacingGameRandomNumberTest -> RaceNumberTest

--- src/main/java/racinggame/domain/RaceNumber.java
+++ src/main/java/racinggame/domain/RaceNumber.java
@@ -1,13 +1,13 @@
 package racinggame.domain;
 
-public class RandomRaceNumber {
+public class RaceNumber {
     public static final int MIN_NO = 0;
     public static final int MAX_NO = 9;
     public static final int BOUNDARY_NO = 4;
 
     private int raceNumber;
 
-    public RandomRaceNumber(int randomNumber) {
+    public RaceNumber(int randomNumber) {
         if(!inValid(randomNumber)) {
             throw new IllegalArgumentException("랜덤 숫자는 0~9의 값이어야 합니다.");
         }
--- src/test/java/racinggame/domain/RaceNumberTest.java
+++ src/test/java/racinggame/domain/RaceNumberTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class RacingGameRandomNumberTest {
+class RaceNumberTest {
     @Test
     @DisplayName("범위보다 낮은 숫자로 볼 생성하기")
     public void 랜덤숫자_생성_실패() {
         assertThatThrownBy(() -> {
-            new RandomRaceNumber(10);
+            new RaceNumber(10);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("랜덤 숫자는 0~9의 값이어야 합니다.");
     }
@@ -25,6 +25,6 @@ class RacingGameRandomNumberTest {
             "4:true",
     }, delimiter = ':')
     void 앞으로_전진(int randomNumber, boolean expected) {
-        assertThat(expected).isEqualTo(new RandomRaceNumber(randomNumber).isPossibleMoveForward());
+        assertThat(expected).isEqualTo(new RaceNumber(randomNumber).isPossibleMoveForward());
     }
 }
